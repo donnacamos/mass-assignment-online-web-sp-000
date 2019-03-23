b6d3b7c475729cd6213cb5cc137ce937880741a2
Donna Amos <donnacamos.developer@gmail.com>
2019-03-23 19:00:23 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,8 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize(avi_attributes) 
-   avi_attributes.each {|key, value| self.send (("#{keys=}"), value)}   
-    
+  def initialize(attributes) 
+   attributes.each do |key, value| self.send (("#{keys=}"), value)   
+  end 
   end 
 end
